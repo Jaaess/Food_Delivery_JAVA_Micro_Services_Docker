f34029b463283303586abc111e955786ec60156e
MontaLabidi <montassar.labidi@ensi-uma.tn>
2018-11-21 16:11:45 +0100

    fix commit

--- src/main/java/com/ensi/ilsi/foodDelivery/Entity/Food.java
+++ src/main/java/com/ensi/ilsi/foodDelivery/Entity/Food.java
@@ -1,18 +1,14 @@
 package com.ensi.ilsi.foodDelivery.Entity;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
 
 
 @Entity
@@ -22,6 +18,7 @@ public class Food {
 	private int id;
     private String name;
     private double price;
+    @ElementCollection
     private List<String> ingredients; 
 	
     
--- src/main/java/com/ensi/ilsi/foodDelivery/Entity/Order.java
+++ src/main/java/com/ensi/ilsi/foodDelivery/Entity/Order.java
@@ -7,8 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Order {
@@ -16,6 +21,9 @@ public class Order {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private HashMap<Food,Integer> orderContent;
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
 	private Customer customer;
 	@Temporal(TemporalType.TIMESTAMP)
     private Date createdOn = new Date();
